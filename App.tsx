f79f5ff21ce3679073f63bbf1dba86716f6eb2e6
chemeria <tonyd.inocentes@gmail.com>
2026-01-23 05:02:59 +0800

    Update App.tsx
    Restore to #19 Commit Jan 20 2026 last stable

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { LiveInterface } from './components/LiveInterface';
-import { ErrorBoundary } from './components/ErrorBoundary';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-slate-950 overflow-hidden">
-      <ErrorBoundary>
-        <LiveInterface />
-      </ErrorBoundary>
+      <LiveInterface />
     </div>
   );
 };
